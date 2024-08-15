9f6620ce3fb97360d0596a9ab54cd995afc63fd3
Cirno <li@hellparadise.jp.net>
2024-08-15 22:36:54 +0700

    fix: armor model on zombie will rotate correctly (still need to fix issue with hand part render twice)

--- src/main/java/lance5057/tDefense/core/tools/armor/renderers/ArmorRenderer.java
+++ src/main/java/lance5057/tDefense/core/tools/armor/renderers/ArmorRenderer.java
@@ -5,9 +5,11 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.Map;
 
@@ -71,6 +73,24 @@ public class ArmorRenderer extends ModelBiped {
             copyModelAngles(this.bipedHead, this.bipedHeadwear);
         } else {
             super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
+            if (entityIn instanceof EntityZombie) {
+                boolean flag = ((EntityZombie)entityIn).isArmsRaised();
+                float f = MathHelper.sin(this.swingProgress * (float)Math.PI);
+                float f1 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * (float)Math.PI);
+                this.bipedRightArm.rotateAngleZ = 0.0F;
+                this.bipedLeftArm.rotateAngleZ = 0.0F;
+                this.bipedRightArm.rotateAngleY = -(0.1F - f * 0.6F);
+                this.bipedLeftArm.rotateAngleY = 0.1F - f * 0.6F;
+                float f2 = -(float)Math.PI / (flag ? 1.5F : 2.25F);
+                this.bipedRightArm.rotateAngleX = f2;
+                this.bipedLeftArm.rotateAngleX = f2;
+                this.bipedRightArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
+                this.bipedLeftArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
+                this.bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+                this.bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
+                this.bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+                this.bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+            }
         }
 
     }
